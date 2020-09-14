c8c104a62a0ba8fed8b255bbf434d5e705648893
xuhuihui <494655043@qq.com>
2020-09-14 20:39:31 +0800

    practice 3 with async

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,7 +1,17 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getPoetry = () => {
-  return fetch("https://v1.jinrishici.com/all.json")
-    .then((response) => response.json())
-    .then(({ content, origin, author }) => [origin, author, content])
-    .catch((error) => console.error(error));
+export const getPoetry = async () => {
+  try {
+    const response = await fetch("https://v1.jinrishici.com/all.json");
+    const { content, origin, author } = await response.json();
+    return [origin, author, content];
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+// export const getPoetry = () => {
+//   return fetch("https://v1.jinrishici.com/all.json")
+//     .then((response) => response.json())
+//     .then(({ content, origin, author }) => [origin, author, content])
+//     .catch((error) => console.error(error));
+// };
