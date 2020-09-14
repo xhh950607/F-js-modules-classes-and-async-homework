caa1781c735d90e862875415eee41f379bbe2b3e
xuhuihui <494655043@qq.com>
2020-09-14 20:31:06 +0800

    practice 3 with Promise

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,7 @@
-export const getPoetry
+// eslint-disable-next-line import/prefer-default-export
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then(({ content, origin, author }) => [origin, author, content])
+    .catch((error) => console.error(error));
+};
