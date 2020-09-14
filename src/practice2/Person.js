d52e098f12f7f0ea18622e4765de826e52c8de71
xuhuihui <494655043@qq.com>
2020-09-14 20:25:21 +0800

    practice 2

--- src/practice2/Person.js
+++ src/practice2/Person.js
@@ -1,1 +1,10 @@
-export class Person {}
+// eslint-disable-next-line import/prefer-default-export
+export class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+}
